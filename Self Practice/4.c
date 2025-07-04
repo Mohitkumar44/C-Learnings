7bbcb824e9f81873709b74861969a8653cd4beeb
Mohit Kumar <mohit.kumar162534@gmail.com>
2025-07-04 12:20:03 +0530

    Macro

--- Self Practice/4.c
+++ Self Practice/4.c
@@ -1,18 +1,29 @@
 // take 3 numbers input and tell if they can be the sides of a triangle
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int main() {
-    int a, b, c;
-    printf("Enter three sides: ");
-    scanf("%d %d %d", &a, &b, &c);
+// int main() {
+//     int a, b, c;
+//     printf("Enter three sides: ");
+//     scanf("%d %d %d", &a, &b, &c);
 
     
-    if (a + b > c && a + c > b && b + c > a) {
-        printf("Yes, they can form a triangle.\n");
-    } else {
-        printf("No, they cannot form a triangle.\n");
-    }
+//     if (a + b > c && a + c > b && b + c > a) {
+//         printf("Yes, they can form a triangle.\n");
+//     } else {
+//         printf("No, they cannot form a triangle.\n");
+//     }
 
+//     return 0;
+// }
+
+
+// macro code
+
+#define statement printf("Hello world");
+#include<stdio.h>
+
+int main(){
+    statement
     return 0;
 }
